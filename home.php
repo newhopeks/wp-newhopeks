83340b43b7777a97fe82a7046b57d41a168c1e23
kbeyer <kyle.beyer@gmail.com>
2011-01-13 23:07:33 -0600

    adding nivo slider files, adding home page for slider testing, adding slider css and js references to header

---
+++ home.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Home Template
+ *
+ * This is the home template.  Technically, it is the "posts page" template.  It is used when a visitor is on the 
+ * page assigned to show a site's latest blog posts.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ */
+
+get_header(); // Loads the header.php template. ?>
+
+	
+    <div id="slider-wrapper">
+        <div id="slider" class="nivoSlider">
+        </div>
+    </div>
+	
+	<div id="content">
+
+		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+					<?php if ( is_front_page() ) { ?>
+						<h2><?php the_title(); ?></h2>
+					<?php } else { ?>	
+						<h1><?php the_title(); ?></h1>
+					<?php } ?>				
+
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
+
+				<?php comments_template( '', true ); ?>
+
+        <?php endwhile; ?>
+    </div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
+
+
+<script>
+// setup variables
+var apiKey = 'a618d5006f9544eb47212249d1c1323f';
+var photosetID = '72157625228479167';
+
+var maxPhotos = 5;
+var sizeCode = 'z';// see suffixes: http://www.flickr.com/services/api/misc.urls.html
+var containerSelector = '#slider';
+
+var apiUrlFormat = 'http://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key={0}&photoset_id={1}&per_page={2}&format=json&jsoncallback=?';
+
+// make request to flickr api 
+$.getJSON(String.format(apiUrlFormat, apiKey, photosetID, maxPhotos), function(data) {
+
+    //loop through the results with the following function
+    $.each(data.photoset.photo, function(i, item) {
+
+        //build the url of the photo in order to link to it
+        var photoURL = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_' + sizeCode + '.jpg';
+
+        var photoID = item.id;
+        var photoTitle = item.title;
+
+        var imgCont = '<img src="' + photoURL + '" alt="' + photoTitle + '"/>';
+
+        //append the 'imgCont' variable to the document
+        $(imgCont).appendTo(containerSelector);
+
+
+        if ((i + 1) == data.photoset.photo.length) {
+            // setup slider after last image
+            $(containerSelector).nivoSlider({
+                effect: 'fade',
+                pauseTime: 5000,
+                pauseOnHover: false
+            });
+        }
+    });
+});
+</script>
---
+++ js/util.js
@@ -0,0 +1,16 @@
+// javascript utility functions
+
+
+// 
+// String extensions
+//
+
+String.format = function () {
+    var s = arguments[0];
+    for (var i = 0; i < arguments.length - 1; i = i + 1) {
+        var reg = new RegExp("\\{" + i + "\\}", "gm");
+        s = s.replace(reg, arguments[i + 1]);
+    }
+
+    return s;
+};
