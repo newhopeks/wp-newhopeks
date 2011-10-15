151629bc103317b0b96d5b9244da254cccabea50
kbeyer <kyle.beyer@gmail.com>
2011-10-15 15:54:06 -0500

    styling blog home page

--- blog-post-list.php
+++ blog-post-list.php
@@ -6,29 +6,46 @@ Template Name: Archives with Content
 
 <?php get_header(); ?>
 
-<div id="content">
- <?php if (have_posts()) : while (have_posts()) : the_post();?>
-<article>
-    <header>
-        <h2 id="post-<?php the_ID(); ?>"><?php the_title();?></h2>
-    </header>
-  
-    <?php the_content(); ?>
-    
- </article>
- <?php endwhile; endif; ?>
- <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+<div id="main" role="main">
+  <section id="page">
+  <?php if (have_posts()) : while (have_posts()) : the_post();?>
+  <div class="widget" id="post">
+		<div>
+			<header><h1><?php the_title();?></h1></header>
+      <?php the_content(); ?>
+  <?php endwhile; endif; ?>
+  <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 
 <h2>Most Recent Posts</h2>
-<ul>
-<?php
-	$args = array( 'numberposts' => '5' );
-	$recent_posts = wp_get_recent_posts( $args );
-	foreach( $recent_posts as $post ){
-		echo '<li><a href="' . get_permalink($post["ID"]) . '" title="Look '.$post["post_title"].'" >' .   $post["post_title"].'</a> </li> ';
-	}
-?>
-</ul>
+<?php 
+    $args = array( 'numberposts' => 10, 'post_type' => 'post' );
+    $lastposts = get_posts( $args );
+    foreach($lastposts as $post) : setup_postdata($post); ?>           
+<article class="event">
+<time datetime="<?php the_time('F jS, Y') ?>"><abbr title="<?php the_time('F') ?>"><?php the_time('M') ?></abbr><?php the_time('j') ?></time>
+<h3><a href="<?php the_permalink(); ?>" 
+    title="<?php the_title() ?>">
+        <?php the_title() ?>
+    </a></h3>
+<p><?php the_excerpt(); ?></p>
+</article>
+<?php endforeach; ?>
+
 </div>
+</div>
+<nav class="secondary">
+
+    <?php
+        // left sidebar holds page nav
+        if ( is_active_sidebar( 'primary-widget-area' ) ) : ?>
+                
+                <?php dynamic_sidebar( 'primary-widget-area' ); ?>
+    
+    <?php endif; ?>
+
+</nav>
+</section>
+
 <?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
